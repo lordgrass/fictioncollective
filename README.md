1963733748a08ae11cc168a8ccd41ceb1d0822e7
Joshua A. <enadasa@vivaldi.net>
2026-04-28 19:04:58 -0500

    Added Light Source Blocks

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# The Fiction Collective Official Luanti Modpack
+#The Fiction Collective Official Luanti Modpack:
 The official code repository of the fiction collective minetest/luanti server
 
-#This modpack contains the following:
-1. Greeter Mod
-2. Utilities Mod
+#This modpack contains the following at this time:
+1. Utilities Mod
--- utilities/init.lua
+++ utilities/init.lua
@@ -6,22 +6,57 @@ local modpath = minetest.get_modpath(modname)
 
 -- Load settings from mod.conf
 local settings = Settings(modpath .. "/mod.conf")
-local enable_glowing_stone = settings:get_bool("enable_glowing_stone", true) -- default ON
+local enable_glowing_nodes = settings:get_bool("enable_glowing_nodes", true)
+local enable_chat_greeter = settings:get_bool("enable_chat_greeter", true)
+-- Greeter for servers
 
--- Greeter (always active)
-minetest.register_on_joinplayer(function(player)
-    local player_name = player:get_player_name()
-    minetest.chat_send_player(player_name, "Welcome to the server, " .. player_name .. "!")
-end)
+local greeting_message = "Welcome to the server!" -- fallback default
 
--- Conditionally register the glowing node
-if enable_glowing_stone then
-    minetest.register_node("enas_utility_mod:stone", {
-        description = "Used for lighting areas where you don't want a torch.",
+local file = io.open(modpath .. "/greeting.txt", "r")
+if file then
+    local content = file:read("*all")
+    file:close()
+    -- Trim whitespace and ensure it's not empty
+    if content and content:match("%S") then
+        greeting_message = content
+    end
+end
+
+if enable_chat_greeter then
+    minetest.register_on_joinplayer(
+	function(player)
+        local name = player:get_player_name()
+        minetest.chat_send_player(name, greeting_message)
+    end
+	)
+end
+
+-- Glowing nodes
+if enable_glowing_nodes then
+    local function creative_only_dig(pos, node, digger)
+		-- Stops survival mode players from breaking the node.
+        if digger and digger:is_player() then
+            local name = digger:get_player_name()
+            if minetest.is_creative_enabled(name) then
+                return minetest.node_dig(pos, node, digger)
+            end
+        end
+    end
+	--Registers the nodes
+	minetest.register_node("utilities:glowing_stone", {
+		description = "Stone | Light Level 14",
         tiles = {"default_stone.png"},
         is_ground_content = false,
         light_source = 14,
         groups = {enas_utilities = 1},
+		on_dig = creative_only_dig,
     })
+	minetest.register_node("utilities:glowing_dirt", {
+	    description = "Dirt | Light Level 14",
+		tiles = {"default_dirt.png"},
+		is_ground_content = false,
+		light_source = 14,
+		groups = {enas_utilities = 1},
+		on_dig = creative_only_dig,
+	})
 end
-
